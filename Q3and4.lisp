f57007779fb97f6f9617121c7f0b25dafd6a2d2b
Darcythiam <darcyirakoze6@gmail.com>
2025-01-28 17:52:13 +0000

    Hmwrk1

---
+++ Q3and4.lisp
@@ -0,0 +1,31 @@
+;; lists
+(defvar list1 '(a b x d))
+(defvar list2 '(a (b (x d))))
+(defvar list3 '(((a (b (x) d)))))
+
+;; Accessing 'x' in (a b x d)
+(print "Accessing 'x' in (a b x d):")
+(print (car (cdr (cdr list1))))
+
+;; Accessing 'x' in (a (b (x d)))
+(print "Accessing 'x' in (a (b (x d))):")
+(print (car (car (cdr (car (cdr list2))))))
+
+;; Accessing 'x' in (((a (b (x) d))))
+(print "Accessing 'x' in (((a (b (x) d)))):")
+(print (car (cdr (car (cdr (car (car list3)))))))
+
+;; Constructing (a b x d)
+(defvar list1 (cons 'a (cons 'b (cons 'x (cons 'd nil)))))
+(print "Constructed (a b x d):")
+(print list1)
+
+;; Constructing (a (b (x d)))
+(defvar list2 (cons 'a (cons (cons 'b (cons (cons 'x (cons 'd nil)) nil)) nil)))
+(print "Constructed (a (b (x d))):")
+(print list2)
+
+;; Constructing (((a (b (x) d))))
+(defvar list3 (cons (cons (cons 'a (cons (cons 'b (cons (cons 'x nil) (cons 'd nil))) nil)) nil) nil))
+(print "Constructed (((a (b (x) d)))):")
+(print list3)
